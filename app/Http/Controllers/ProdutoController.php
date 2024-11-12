56a84beac9c3ae9bd80befea036f7a2a9fee1e43
William Firmino <williams.an@hotmail.com>
2024-11-12 19:51:01 -0300

    Alteração com o ProdutoController

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
-    //
+    public function create() {
+        return view('produtos.create');
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'nome'=>
+            'descricao'=>
+            'tamanho'=>
+            'cor'=>
+            'preco'=>
+            'quantidade'=>
+            'imagem'=>
+        ])
+    }
 }
